2fac0a0e0b00021a31b42b8e87b57e64eb9a2b71
lukasevicromuald <lukasevicromuald@gmail.com>
2022-08-22 18:28:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P038_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+               1- Sukurkite klases Hobby su properčiais
+                  - Id 
+                  - Text
+                  - TextLt
+               2- Sukurkite konstruktorių be parametrų (pasiekiama visur)
+               3- Sukurkite konstruktorių su id, text, textLt parametrais (pasiekiama visur)
+               4- Skirtingais būdais inicializuokite  klases. Įrašykite po 3 hobius.
+               5- Hobby klasėje sukurkite metodą kuris iškoduos HobbyInitialData.DataSeedCsv vieną įrašą (string)
+                  ir užpildys properčius duomenimis. unit-test
+           */
+
+
+        }
+    }
+}
